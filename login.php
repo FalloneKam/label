f64a207136522e2995efe665e19e4e9c3167de42
Frederic Etotobo <etotobo@gmail.com>
2016-05-07 14:08:26 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php 
+/* Page de connexion incontournable pour accéder aux données */
+
+// Cas où l'utilisateur est déjà connecté
+// On le redirige vers la page d'acceuil
+
+// Autre cas
+//On affiche le formulaire de connexion
+
+?>
