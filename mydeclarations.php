90c2fd0fc2cff4a37e2693a8b22bf6d6d020f1bb
Frederic Etotobo <etotobo@gmail.com>
2016-05-07 14:37:51 +0200

    Create mydeclarations.php

---
+++ mydeclarations.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Page qui permet à chaque partie d'afficher la liste de ses déclarations
+et de les télécharger au format pdf. De la façon suivante :
+- l'annonceur pourra voir la liste de ses déclarations
+- la régie pourra voir sa liste de déclarations
+- le partenaire (communauté urbaine) pourra voir la liste des déclarations de sa zone
+- Label pourra voir la liste de toutes les déclarations
+*/
+
+?>
