9f1f9bc7437f5a32c8605b3b96f640259dd5a702
Frederic Etotobo <etotobo@gmail.com>
2016-05-07 14:11:14 +0200

    Create login-traitement.php

---
+++ login-traitement.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+Page invisible qui permet de traiter l'information renvoyée
+par le formulaire de connexion
+*/
+
+// Cas où l'utilisateur est déjà connecté
+// On le renvoi vers la page d'acceuil
+
+// Cas où c'est Label
+// Il pourra accéder à tous
+
+//Cas où c'est un partenaire de Label
+
+//Cas où c'est une régie
+
+//Cas où c'est un annonceur VIP
+
+// Cas où c'est un annonceur basique
+
+?>
