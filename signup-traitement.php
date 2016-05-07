597da6ec256260778d1ec143389d3e02f37faae9
Frederic Etotobo <etotobo@gmail.com>
2016-05-07 14:15:04 +0200

    Create signup-traitement.php

---
+++ signup-traitement.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+Page invisible qui permet de traiter l'information renvoyée
+par le formulaire d'inscription
+*/
+
+// Cas où l'utilisateur est déjà connecté
+// On le renvoi vers la page d'acceuil
+
+// Cas où il a choisi de s'inscrire comme partenaire de Label
+
+// Cas où il a choisi de s'inscrire comme une régie
+
+// Cas où il a choisi de s'inscrire comme un annonceur VIP
+
+// Cas où il a choisi de s'inscrire comme un annonceur basique
+
+?>
