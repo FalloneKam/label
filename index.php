69094bad4febf5b69a4b5d1331092eb3b06d3e04
Frederic Etotobo <etotobo@gmail.com>
2016-05-07 14:20:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php 
-//ici on écrira le code php
+/*  
+Cette page c'est la page d'acceuil officielle
+qui sera une sorte de landing-page unique avec toutes les infos nécessaires dessus.
+Les menus renverront vers des ancres de la même page.
+*/
 
 ?>
