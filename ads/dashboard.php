2bdd62a0f14e60a45c7f34f353ed5c42ef2127a0
Frederic Etotobo <etotobo@gmail.com>
2016-05-07 16:45:50 +0200

    Create dashboard.php

---
+++ ads/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Page affichant le tableau de bord des publicités avec des liens vers :
+- Toutes les campagnes (visualiser toutes les campagnes d'affichage prévues, en cours et terminées)
+- mes campagnes (visualiser les détails de ses campagnes d'affichage prévues, en cours et terminées)
+- supports libres
+- Simulation
+
+*/
+
+?>
