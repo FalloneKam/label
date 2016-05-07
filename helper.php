b562bb2c915b0a828c4659cc55062834897c56d2
Frederic Etotobo <etotobo@gmail.com>
2016-05-07 14:42:58 +0200

    Update helper.php

--- helper.php
+++ helper.php
@@ -3,4 +3,8 @@
 Ici seront regroupé toutes les fonctions importante qu'on a choisi d'externaliser
 */
 
+//bdConnect() : Fonction qui permet de se connecter à la base de données
+
+//statCalculator() : Fonction qui permet de calculer les stats liées aux déclarations
+
 ?>
