0c343e6e1fdcceba5551db1eacc6f1f09dd64fe1
falloneKAM <lonefalkeys@gmail.com>
2016-05-17 22:27:14 +0100

    Test Git Commit

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ Cette page c'est la page d'acceuil officielle
 qui sera une sorte de landing-page unique avec toutes les infos nécessaires dessus.
 Les menus renverront vers des ancres de la même page.
 */
-
+// Test GIT
 ?>
